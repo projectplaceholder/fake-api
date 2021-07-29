a093326f97641dc804600c4b0d594b79ef84026e
MichaBrugger <michabrugger14@gmail.com>
2021-07-29 17:42:17 +0200

    required library 'moment' added

--- generators/hsg-bidding-fakeEvents.js
+++ generators/hsg-bidding-fakeEvents.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const faker = require("faker");
 const amount_courses = 600;
 //const amount_weeks = 14;
@@ -8,14 +9,17 @@ const func = (next) => (create) => {
     var hsgBiddingFakeEvents = [];
 
     for (var course_id = 0; course_id < amount_courses; course_id++) {
+    
+    // defining the start moment and the random length of courses (in 30 min intervals)
     var startdate = new Date('2021-09-13T08:00');
     var length = (Math.floor(Math.random() * 3) + 1) * 30;
 
-
-    startdate.setTime(startdate.getTime()+Math.floor(Math.random()*8)*60*60*1000);
-    startdate.setTime(startdate.getTime()+Math.floor(Math.random()*5)*24*60*60*1000);
-    enddate = (startdate.getTime()+length*60*1000);
+    // adding random days (0-5) and hours (0-8) to the start moment
+    startdate.getTime()+Math.floor(Math.random()*8)*60*60*1000;
+    startdate.getTime()+Math.floor(Math.random()*5)*24*60*60*1000;
+    enddate = startdate.getTime()+length*60*1000;
     
+    // converting to YYYY-MM-DD HH:mm
     final_start = moment.utc(startdate).format('YYYY-MM-DD HH:mm');
     final_end = moment.utc(enddate).format('YYYY-MM-DD HH:mm');
 
@@ -23,6 +27,8 @@ const func = (next) => (create) => {
         "id": course_id,
         "starttime": final_start,
         "endtime": final_end,
+        "raw_start": startdate,
+        "raw_end": enddate,
       });
     }
     return { "hsg-bidding-fakeEvents": hsgBiddingFakeEvents };
