e350e0b9e86ea01f69be6c117afd3ff7ead9fa23
MichaBrugger <michabrugger14@gmail.com>
2021-08-01 13:59:46 +0200

    start and end with proper format

--- generators/hsg-bidding-fakeEvents.js
+++ generators/hsg-bidding-fakeEvents.js
@@ -10,19 +10,19 @@ const func = (next) => (create) => {
 
     for (var course_id = 0; course_id < amount_courses; course_id++) {
     
-    // defining the start moment and the random length of courses (in 30 min intervals)
-    var startdate = new Date('2021-07-26T08:00');
-    var enddate = startdate;
-    // var length = (Math.floor(Math.random() * 3) + 1) * 30;
+      // defining the start moment and the random length of courses (in 30 min intervals)
+      var date = new Date('2021-07-26T08:00');
+      var rnd_days = parseInt(Math.floor(Math.random()*8)*60*60*1000);
+      var rnd_hours = parseInt(Math.floor(Math.random()*5)*24*60*60*1000);
+      var rnd_length = parseInt((Math.floor(Math.random() * 3) + 1) * 30);
 
-    // adding random days (0-5) and hours (0-8) to the start moment
-    startdate.getTime()+Math.floor(Math.random()*8)*60*60*1000;
-    startdate.getTime()+Math.floor(Math.random()*5)*24*60*60*1000;
-    var enddate = startdate.getTime()+(Math.floor(Math.random() * 3) + 1) * 30
-    
-    // converting to YYYY-MM-DD HH:mm
-    //final_start = moment.utc(startdate).format('YYYY-MM-DD HH:mm');
-    //final_end = moment.utc(enddate).format('YYYY-MM-DD HH:mm');
+      // adding random days (0-5) and hours (0-8) to the start moment
+      var startdate = new Date(date.setTime(date.getTime()+rnd_days+rnd_hours)).toISOString();
+      var enddate = new Date(date.setTime(date.getTime()+rnd_days+rnd_hours+rnd_length)).toISOString();
+      
+      // converting to YYYY-MM-DD HH:mm
+      //final_start = moment.utc(startdate).format('YYYY-MM-DD HH:mm');
+      //final_end = moment.utc(enddate).format('YYYY-MM-DD HH:mm');
 
       fakeEvents.push({
         "id": course_id,
